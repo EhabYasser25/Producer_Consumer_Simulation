ca18725f9ca8a7a32cbb74df8205e18c5a6a89a3
Random <ehabflash833@gmail.com>
2023-01-26 18:23:18 +0200

    add back

---
+++ Simulation_Back/src/test/java/com/simulation/Simulation_Back/SimulationBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simulation.Simulation_Back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimulationBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
